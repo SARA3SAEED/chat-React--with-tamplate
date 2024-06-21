710862af5b9fb85c6399d3f6d6d463f73ece78fe
SARA3SAEED <Swaretoo@gmail.com>
2024-06-21 08:16:40 +0300

    1 commit

--- src/ChatWindow.jsx
+++ src/ChatWindow.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-import './ChatWindow.css';
 
 const ChatWindow = ({ user, messages, onSendMessage }) => {
   const [newMessage, setNewMessage] = useState('');
